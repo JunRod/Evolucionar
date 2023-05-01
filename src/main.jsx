beff792dc21a40a540f0b4ac16cbd40e31ac7826
JuNRod <junrodpro@gmail.com>
2023-05-01 15:52:58 -0500

    meta

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 import { store } from "./store";
 import { MasculinidadApp } from "./MasculinidadApp";
-import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
