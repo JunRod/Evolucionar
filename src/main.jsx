46ad3a8122d58ceab08d7f62e43e16907cda4612
JuNRod <junrodpro@gmail.com>
2023-05-01 11:42:00 -0500

    analitics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import { MasculinidadApp } from './MasculinidadApp'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { MasculinidadApp } from "./MasculinidadApp";
+import { Analytics } from "@vercel/analytics/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <MasculinidadApp />
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <MasculinidadApp />
+    <Analytics />
+  </Provider>
+);
